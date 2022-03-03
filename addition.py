979cfc04b429f54eef9496ce9482e5a6f29df997
HarvinderSinghDiwan <46323744+HarvinderSinghDiwan@users.noreply.github.com>
2022-03-04 01:27:14 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  2 04:05:49 2022
-
-@author: Harvi
-"""
 from flask import Flask, request, jsonify,Response
 app = Flask(__name__)
 @app.route("/addition")
